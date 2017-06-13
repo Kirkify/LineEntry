edef8bbbc81a62b620bc68db0d8ebf4347db61c3
Kirk Davies <davi0534@algonquinlive.com>
2017-06-13 15:20:01 -0400

    First commit

---
+++ LineEntry.FormsPlugin.Droid/LineEntryRenderer.cs
@@ -0,0 +1,40 @@
+﻿using Android.Graphics;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace LineEntry.FormsPlugin.Droid
+{
+  public class LineEntryRenderer : EntryRenderer
+  {
+    protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+    {
+      base.OnElementChanged(e);
+
+      if (Control != null)
+      {
+        var defaultColorFilter = Control.Background.ColorFilter;
+        var element = Element as LineEntry;
+        Control.Background.SetColorFilter(element.BorderColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+        Control.SetPadding(Control.PaddingLeft + (element.IconPadding ? 68 : 0), Control.PaddingTop, Control.PaddingRight, Control.PaddingBottom);
+
+      }
+    }
+
+    protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+    {
+      base.OnElementPropertyChanged(sender, e);
+
+      var element = Element as LineEntry;
+      if (e.PropertyName == LineEntry.BorderColorProperty.PropertyName)
+      {
+        Control.Background.SetColorFilter(element.BorderColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+      }
+      if (e.PropertyName == LineEntry.IconPaddingProperty.PropertyName)
+      {
+        Control.SetPadding(Control.PaddingLeft + (element.IconPadding ? 68 : 0), Control.PaddingTop, Control.PaddingRight, Control.PaddingBottom);
+      }
+    }
+
+  }
+}
